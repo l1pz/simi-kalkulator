03b8200d699b188c0a1f13d861df605ed6bf18dd
Máté Molnár <mmateka89@gmail.com>
2020-12-22 18:45:56 +0100

    tried fixing offline cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,16 +6,21 @@ const CACHE_NAME = 'static-cache-v1';
 // Add list of files to cache here.
 const FILES_TO_CACHE = [
   '/offline.html',
+  '/build/bundle.css',
+  '/build/bundle.js',
+  '/images/bg.png',
+  '/images/logo.png',
+  '/index.html',
 ];
 
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
 
   evt.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => {
-        console.log('[ServiceWorker] Pre-caching offline page');
-        return cache.addAll(FILES_TO_CACHE);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
 
   self.skipWaiting();
@@ -25,14 +30,14 @@ self.addEventListener('activate', (evt) => {
   console.log('[ServiceWorker] Activate');
   // Remove previous cached data from disk.
   evt.waitUntil(
-      caches.keys().then((keyList) => {
-        return Promise.all(keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log('[ServiceWorker] Removing old cache', key);
-            return caches.delete(key);
-          }
-        }));
-      })
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
   );
 
   self.clients.claim();
@@ -46,12 +51,12 @@ self.addEventListener('fetch', (evt) => {
     return;
   }
   evt.respondWith(
-      fetch(evt.request)
-          .catch(() => {
-            return caches.open(CACHE_NAME)
-                .then((cache) => {
-                  return cache.match('offline.html');
-                });
-          })
+    fetch(evt.request)
+      .catch(() => {
+        return caches.open(CACHE_NAME)
+          .then((cache) => {
+            return cache.match('index.html');
+          });
+      })
   );
 });
